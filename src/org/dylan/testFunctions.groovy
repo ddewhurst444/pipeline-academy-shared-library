a94360cfede755f932aa82f127aa65ecfd2d1137
Dylan Dewhurst <ddewhurst@cloudbees.com>
2018-08-14 12:11:46 -0600

    First shared library function

---
+++ src/org/dylan/testFunctions.groovy
@@ -0,0 +1,12 @@
+package org.dylan;
+
+class testFuncs {
+
+    static def setEnv(script, envString) {
+        ${script.env.TEST = envString}
+      }
+
+    /*def multiply(int a, int b) {
+        return  a * b
+    }*/
+}
