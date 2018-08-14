c43aa62e05fffa0767e8f1db7ca22b60a8c2b140
Dylan Dewhurst <ddewhurst@cloudbees.com>
2018-08-14 13:27:43 -0600

    added constructor

--- src/org/dylan/TestFuncs.groovy
+++ src/org/dylan/TestFuncs.groovy
@@ -5,6 +5,8 @@ class TestFuncs {
     /*static def setEnv(script, envString) {
         ${script.env.TEST = envString}
       }*/
+    //def steps
+    TestFuncs() {}
 
     def multiply(int a, int b) {
         return  a * b
